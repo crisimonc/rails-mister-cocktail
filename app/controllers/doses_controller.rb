4677140e5841c2b6a603df7956ae1ae7e1a6c973
Cristina Simoncini <crisimonc@gmail.com>
2018-10-25 19:07:18 +0200

    Mister Cocktail App, style to be added

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,13 +5,21 @@ class DosesController < ApplicationController
   end
 
   def create
-  	@cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(cocktail: Cocktail.find(params[:cocktail_id]), ingredient: Ingredient.find(params[:dose]["ingredient_id"]), description: params[:dose]["description"])
     @dose.save
     redirect_to cocktail_path(@cocktail)
   end
 
+  def destroy
+  	@cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
   private
+
   def dose_params
     params.require(:dose).permit(:description)
   end
